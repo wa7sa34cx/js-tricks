a3266a5c58ac6d399b84c659145ae956ac45e28f
Alex Black <wa7@sa34.cx>
2021-03-13 16:50:32 +0300

    Flat Array

--- 03-flat-array.js
+++ 03-flat-array.js
@@ -1,1 +1,17 @@
-"use strict";
+'use strict';
+
+const multiArr = [1, [1, 2, [3, 4]], [2, 4], 5];
+
+const flatArr = arr => {
+  let newArr = [];
+
+  arr.forEach(element => {
+    newArr = newArr.concat(
+      typeof element === 'object' ? flatArr(element) : element
+    );
+  });
+
+  return newArr;
+};
+
+console.log(flatArr(multiArr));
